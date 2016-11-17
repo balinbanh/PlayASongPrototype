b45b96eeed906e84d60c8c2b4672d50b7d9a6fa9
b2banh <b2banh@ryerson.ca>
2016-11-17 13:44:59 -0500

    Setup for playlist arrays added.

--- PlayASongPrototype/WorkingArea.vb
+++ PlayASongPrototype/WorkingArea.vb
@@ -1,6 +1,15 @@
 ﻿Public Class WorkingArea
     Dim songNames() As String = {"babablacksheep", "littleMonkeys", "happy", "maryLittleLamb", "wheels"}
-    Private Sub WorkingArea_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Dim songs As New Dictionary(Of String, String)
+    Dim playlistnames() As String
+    Dim playlistsongs(,) As String
 
+
+    Private Sub WorkingArea_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        songs.Add("babablcksheep", "")
+        songs.Add("littleMonkeys", "")
+        songs.Add("happy", "")
+        songs.Add("maryLittleLamb", "")
+        songs.Add("wheels", "")
     End Sub
 End Class
