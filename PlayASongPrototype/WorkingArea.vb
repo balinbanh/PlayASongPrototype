d79a21d303f419303c355ea2a446da2e0730b028
b2banh <b2banh@ryerson.ca>
2016-11-17 14:58:52 -0500

    Playlist create, remove, addsong, removesong functions added.

--- PlayASongPrototype/WorkingArea.vb
+++ PlayASongPrototype/WorkingArea.vb
@@ -1,9 +1,34 @@
 ﻿Public Class WorkingArea
-    Dim songNames() As String = {"babablacksheep", "littleMonkeys", "happy", "maryLittleLamb", "wheels"}
-    Dim songs As New Dictionary(Of String, String)
-    Dim playlistnames() As String
-    Dim playlistsongs(,) As String
+    Dim songNames() As String = {"babablacksheep", "littleMonkeys", "happy", "maryLittleLamb", "wheels"} 'full list of songs
+    Dim songs As New Dictionary(Of String, String) 'songs, corresponding name of song image
+    Dim playlistnames As New List(Of String) 'names of playlists
+    Dim playlistsongs As New List(Of List(Of String)) 'list of song lists'
 
+
+    Private Sub createPlaylist(ByVal playlistName As String)
+        If playlistnames.Contains(playlistName) Then
+            playlistnames.Add(playlistName)
+        End If
+
+    End Sub
+
+    Private Sub removePlaylist(ByVal playlistname As String)
+        If playlistnames.Contains(playlistname) Then
+            playlistnames.Remove(playlistname)
+        End If
+    End Sub
+
+    Private Sub addSong(ByVal playlistName As String, ByVal songName As String)
+        If playlistnames.Contains(playlistName) And songNames.Contains(songName) Then
+            playlistsongs(playlistnames.BinarySearch(playlistName)).Add(songName)
+        End If
+    End Sub
+
+    Private Sub removeSong(ByVal playlistName As String, ByVal songName As String)
+        If playlistnames.Contains(playlistName) And songNames.Contains(songName) Then
+            playlistsongs(playlistnames.BinarySearch(playlistName)).Remove(songName)
+        End If
+    End Sub
 
     Private Sub WorkingArea_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         songs.Add("babablcksheep", "")
